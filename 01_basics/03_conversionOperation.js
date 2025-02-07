015b476e347cbbbfccaf9b1554dab5c4da4e0511
aditi0904 <aditisalunkhe3021@gmail.com>
2025-02-07 13:27:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"aditi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+//******Operations *******
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  //agar string first hai to sabhi ko string consider kr diya jayega agar last hai to operation perform hoga and then string
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
